823abb0a2b1a374d4ee64b6d374602f1bb5c7232
gregmarquet <marquet.greg@gmail.com>
2016-02-06 20:50:52 -0500

    Creates classes, getter and setter methods

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+  def name=(name)  # Setter method
+    @name = name
+  end
+  def name  # Getter method
+    @name
+  end
+  def breed=(breed)  # Setter method
+    @breed = breed
+  end
+  def breed  # Getter method
+    @breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+
+  def name=(name)  # Setter method
+    @name = name
+  end
+  def name   # Getter method
+    @name
+  end
+  def job=(job)   # Setter method
+    @job = job
+  end
+  def job   # Getter method
+    @job
+  end
+
+end
